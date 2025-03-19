3de6ab0290c7fd4b8be9d2bcc34c8fa64ca07802
Jan Kowalski <pankiewiczjakub@gmail.com>
2025-03-19 11:51:03 +0100

    Pierwszy zapis do repozytorium Lab5

---
+++ src/pl/pp/mojaPiataAplikacja.java
@@ -0,0 +1,134 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+/* public class mojaPiataAplikacja {
+    public static void main(String[] args) {
+
+        // Obliczenia i wyświetlenie wyniku dla wartości przypisanych w kodzie aplikacji
+        System.out.println("Obliczenia i wyświetlenie wyniku dla wartości przypisanych w kodzie aplikacji");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Twoj wynik #1 to " + finalScore);
+        }
+
+        // Obliczenia i wyświetlenie wyniku dla wartości przypisanych w kodzie aplikacji (innych niż poprzednio)
+        System.out.println("Obliczenia i wyświetlenie wyniku dla wartości przypisanych w kodzie aplikacji (innych niż poprzednio)");
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+
+        finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Twoj wynik #2 to " + finalScore);
+        }
+
+        // Obliczenia za pomocą metody calculateScore_noArguments(), ale nadal dla wartości przypisanych w kodzie metody
+        System.out.println("Obliczenia za pomocą metody calculateScore_noArguments(), ale nadal dla wartości przypisanych w kodzie metody");
+        calculateScore_noArguments();
+
+        // Obliczenia za pomocą metody calculateScore(), ale tym razem dla wartości wpisanych do argumentu wywołania metody
+        System.out.println("Obliczenia za pomocą metody calculateScore(), ale tym razem dla wartości wpisanych do argumentu wywołania metody");
+        calculateScore_arguments(true, 2500, 9, 2500);
+        // można to wywołać za pomocą wcześniej zadeklarowanych zmiennych
+        calculateScore_arguments(gameOver, score, levelCompleted, bonus);
+        // ale widzicie, że wyświetlanie funkcji jest również z numerkiem #4 (bo takie wyświetlanie jest zapisane
+        // w metodzie. Co zrobić, żeby tak nie było? Poniżej:
+
+        // Obliczenia i wyświetlanie można uniezależnić od siebie dodając parametr, który ta metoda będzie zwracać
+        System.out.println("Obliczenia i wyświetlanie można uniezależnić od siebie dodając parametr, który ta metoda będzie zwracać za pomocą return");
+        finalScore = calculateScore_argumentsReturn(gameOver, score, levelCompleted, bonus);
+        System.out.println("Twoj wynik #5 to " + finalScore);
+    }
+
+    private static void calculateScore_noArguments() {
+        boolean gameOver = true;
+        int score = 150;
+        int levelCompleted = 2;
+        int bonus = 1000;
+
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Twoj wynik #3 to " + finalScore);
+        }
+    }
+    private static void calculateScore_arguments(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Twoj wynik #4 to " + finalScore);
+        }
+    }
+    private static int calculateScore_argumentsReturn(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+        }
+        return finalScore;
+    }
+}
+*/
+public class mojaPiataAplikacja {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int kolumna;
+        while (true) {
+            System.out.println("Podaj liczbę kolumn tabeli:");
+            kolumna = scanner.nextInt();
+            if (kolumna > 0) {
+                break;
+            }
+            System.out.println("Liczba musi być większa od zera");
+        }
+        int wiersz;
+        while (true) {
+            System.out.println("Podaj liczbę wierszy tabeli:");
+            wiersz = scanner.nextInt();
+            if (wiersz > 0) {
+                break;
+            }
+            System.out.println("Liczba musi być większa od zera");
+        }
+
+            System.out.println("Podaj znak z którego ma być zrobiona tabela:");
+            //użyłem bez inta zeby dało się wpisać dowolny znak
+            String znak = scanner.next();
+
+            System.out.println("Twoja kolumna wygląda tak:");
+            int wiersz2=wiersz;
+            while (wiersz2>0) {
+
+                int kolumna2=kolumna;
+                while (kolumna2>0) {
+                    System.out.print(znak + " ");
+                    kolumna2--;
+                }
+                System.out.println();
+                wiersz2--;
+            }
+
+            scanner.close();
+        }
+
+
+    }
+
+
+
+
+
+
